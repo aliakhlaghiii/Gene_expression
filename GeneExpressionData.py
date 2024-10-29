f2929dc56057eadd849bf3fd873ba17d562e1c01
aliakhlaghiii <142217331+aliakhlaghiii@users.noreply.github.com>
2024-10-29 14:07:54 +0100

    Update GeneExpressionData.py

--- GeneExpressionData.py
+++ GeneExpressionData.py
@@ -1,4 +1,4 @@
-'''In this class I try to manipulate, store and read the data'''
+'''In this class, I try to manipulate, store and read the data'''
 class GeneExpressionData:
     def __init__(self, file_path):   # initialize with file path
         self.file_path = file_path
@@ -8,29 +8,29 @@ class GeneExpressionData:
 
     '''function to read the file and store data'''
     def read_file(self):
-        with open(self.file_path, 'r', encoding='ascii') as file:
+        with open(self.file_path, 'r', encoding='ascii') as file:   # open file in read mode with ascii encoder
             lines = file.readlines()   # read the whole lines
-            headers = lines[0].strip().split(',')   # assume the first line splitted by ',' as header
+            headers = lines[0].strip().split(',')   # assume the first line split by ',' as the header
 
             '''in the header, from the third cell to the end, I assumed to gene name(header). each column has
-             expressions and values for each gene. so, I'm going to make an empty list(as values of dictionary) to
-              scan the rows one by one, and based on its index, I allocated them to each gene name. finally,
+             expressions and values for each gene. so, I'm going to make an empty list(as values of the dictionary) to
+              scan the rows one by one, and based on its index, I allocate them to each gene name. finally,
               I have columns with a header(gene name)'''
-            self.gene_exp = {header: [] for header in headers[2:]}
+            self.gene_exp = {header: [] for header in headers[2:]}   # Creates an empty list for value of each header
 
             for line in lines[1:]:   # read the file line by line
                 values = line.strip().split(',')
-                sample_type = values[1]   # assume the second column as gene type
+                sample_type = values[1]   # Assume the second column is gene type
 
                 ''' complete the dictionary with headers(keys) and append the values to that empty list '''
                 for key, value in zip(headers[2:], values[2:]):
-                    self.gene_exp[key].append(float(value))
+                    self.gene_exp[key].append(float(value))   # append the floated values as value of gene_exp dictionary
 
                 if sample_type == 'normal':   # check the type
                     for key, value in zip(headers[2:], values[2:]):
                         ''' if gene name doesn't exist, make an empty list to put the values in it '''
                         if key not in self.normal_dict:
-                            self.normal_dict[key] = []   # make a new key
+                            self.normal_dict[key] = []   # if the gene name does not exist, make a new one
                         ''' if gene name exists, just append its values '''
                         self.normal_dict[key].append(float(value))   # if it exists, append it
                 elif sample_type == 'HCC':   # as same as Normal, I checked HCC conditions
@@ -38,5 +38,5 @@ class GeneExpressionData:
                         if key not in self.hcc_dict:
                             self.hcc_dict[key] = []   # make a new key
                         self.hcc_dict[key].append(float(value))   # if it exists, append it
-        ''' return the values. we don't need to return samples but I do it to reassurance '''
-        return self.gene_exp, self.normal_dict, self.hcc_dict
+        ''' return the values. we don't need to return samples but I do it for reassurance '''
+        return self.gene_exp, self.normal_dict, self.hcc_dict   # after the end of each loop, return the value of variables.
