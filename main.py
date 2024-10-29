bf1b1da681347cbee9928d6f22fbd9c3c705f7b2
aliakhlaghiii <142217331+aliakhlaghiii@users.noreply.github.com>
2024-10-29 15:39:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-'''from GeneExpressionData file, import GeneExpressionData class, since the class name  is as same as the file name'''
+import argparse   # import argparse to handle the command line arguments
+'''since the class name is as same as the file name, I used from-import'''
 from GeneExpressionData import GeneExpressionData
 from StatisticalAnalysis import StatisticalAnalysis
 from AnalysisReport import AnalysisReport
 
-if __name__ == "__main__":   # run directly
-    try:   # I used try-except in order to if user enters a wrong path file, the system shows an standard error
-        path_file = input("Enter the file path: ")
-        destination = input("Enter output destination (type 'screen' for screen or provide file path): ")
-
+def main(file_path, destination):   # main of the program
+    try:   # if the file path and destination are correct and exist, try.
         # Initialize classes and read data
-        gene_data = GeneExpressionData(path_file)
-        gene_data.read_file()
-        analysis = StatisticalAnalysis(gene_data)
-        report = AnalysisReport(destination)  # report is obj of AnalysisReport class that initialized by a destination
+        gene_data = GeneExpressionData(file_path)   # gene_data is an object of GeneExpressionData class
+        gene_data.read_file()   # call read_file function 
+        analysis = StatisticalAnalysis(gene_data)   # make an object of StatisticalAnalysis class named analysis.
+        report = AnalysisReport(destination)   # make an object of AnalysisReport class named report.
 
         # Display analysis report
-        report.display(analysis)
+        report.display(analysis)   # call display method in report object
 
-    except FileNotFoundError as e:
-        print(e)
+    except FileNotFoundError as e:   # if the file path does not exist, it shows a standard error
+        print(f"Error: {e}")
+
+if __name__ == "__main__":   # run directly
+    # Initialize ArgumentParser object(parser)
+    parser = argparse.ArgumentParser(description="Analyse gene expression data and perform some statistical analysis.")
+    
+    '''with add_argument method, I determine the positional arguments. In this case, both of them are str'''
+    parser.add_argument("file_path", type=str, help="Path to the gene expression data file.")
+    parser.add_argument("destination", type=str, help="Output destination (type 'screen' for screen output or provide file path).")
+
+    args = parser.parse_args()   # according to the arguments above, they are stored in args
+
+    # main function with parsed arguments
+    main(args.file_path, args.destination)
