b105a19a2a0151a5b12c515d1efe105dd81a1d17
aliakhlaghiii <142217331+aliakhlaghiii@users.noreply.github.com>
2024-10-30 19:31:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import argparse   # import argparse to handle the command line arguments
-'''since the class name is as same as the file name, I used from-import'''
 from GeneExpressionData import GeneExpressionData
 from StatisticalAnalysis import StatisticalAnalysis
 from AnalysisReport import AnalysisReport
 
-def main(file_path, destination):   # main of the program
-    try:   # if the file path and destination are correct and exist, try.
-        # Initialize classes and read data
+def main(file_path, destination, gene_name, threshold):  # main with 4 parameters
+    try:
         gene_data = GeneExpressionData(file_path)   # gene_data is an object of GeneExpressionData class
-        gene_data.read_file()   # call read_file function 
+        gene_data.read_file()   # call read_file function
         analysis = StatisticalAnalysis(gene_data)   # make an object of StatisticalAnalysis class named analysis.
         report = AnalysisReport(destination)   # make an object of AnalysisReport class named report.
 
         # Display analysis report
-        report.display(analysis)   # call display method in report object
+        report.display(analysis, gene_name, threshold)  # Pass gene_name and threshold
 
     except FileNotFoundError as e:   # if the file path does not exist, it shows a standard error
-        print(f"Error: {e}")
+        print(e)
 
 if __name__ == "__main__":   # run directly
-    # Initialize ArgumentParser object(parser)
+# Initialize ArgumentParser object(parser), also the user can see this description when the user enter -h for help'''
     parser = argparse.ArgumentParser(description="Analyse gene expression data and perform some statistical analysis.")
-    
-    '''with add_argument method, I determine the positional arguments. In this case, both of them are str'''
+
+    # Define positional arguments
     parser.add_argument("file_path", type=str, help="Path to the gene expression data file.")
     parser.add_argument("destination", type=str, help="Output destination (type 'screen' for screen output or provide file path).")
+    parser.add_argument("gene_name", type=str, help="Gene name to analyze.")
+    parser.add_argument("threshold", type=float, help="Threshold for gene expression filtering.")
 
-    args = parser.parse_args()   # according to the arguments above, they are stored in args
+    # Parse arguments respectively
+    args = parser.parse_args()
 
-    # main function with parsed arguments
-    main(args.file_path, args.destination)
+    # Call main function with parsed arguments
+    main(args.file_path, args.destination, args.gene_name, args.threshold)
